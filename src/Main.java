54f4fcbe2cffd62ff132c0ee96a0014fc52a2b14
Robin-Guragain <robinguragain@gmail.com>
2024-05-16 19:33:21 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 100;
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        FizzBuzz.main(cars);
+
+    }
+
+}
